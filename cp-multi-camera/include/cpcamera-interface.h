302f49468962931a2a03cfe497d25ca6a7dfa57b
Pai Peng <sipaipv6@gmail.com>
2022-07-19 12:59:46 +0800

    add cp-multi-camera headers/library

---
+++ cp-multi-camera/include/cpcamera-interface.h
@@ -0,0 +1,15 @@
+﻿#ifndef CAMERAINTERFACE_H
+#define CAMERAINTERFACE_H
+
+
+#include <QPixmap>
+
+class CPCameraInterface {
+public:
+    virtual ~CPCameraInterface() {}
+    virtual void cameraState(int cameraId, int state) = 0;
+    virtual void processCapturedImage(int cameraId, const QImage& img) = 0;
+    virtual void cameraReadyForCapture(int cameraId, bool ready) = 0;
+};
+
+#endif // CAMERAINTERFACE_H
---
+++ cp-multi-camera/include/cpcameraviewfinder.h
@@ -0,0 +1,19 @@
+﻿#ifndef CPCAMERAVIEWFINDER_H
+#define CPCAMERAVIEWFINDER_H
+
+#include <QCameraViewfinder>
+
+#include "cp-multi-camera_global.h"
+
+class CPMULTICAMERA_EXPORT CPCameraViewfinder : public QCameraViewfinder
+{
+    Q_OBJECT
+public:
+    CPCameraViewfinder(QWidget *parent = nullptr);
+    void setText(const QString &text);
+
+protected:
+    void paintEvent(QPaintEvent* event) override;
+};
+
+#endif // CPCAMERAVIEWFINDER_H
