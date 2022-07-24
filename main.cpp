bf3864bbcef4a1466896dbc607adcf9d2c7542d8
Pai Peng <sipaipv6@gmail.com>
2022-07-24 09:59:44 +0800

    redirect qDebug to file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,42 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
 
+// Get the default Qt message handler.
+// static const QtMessageHandler QT_DEFAULT_MESSAGE_HANDLER = qInstallMessageHandler(0);
+
+void myCustomMessageHandler(QtMsgType type, const QMessageLogContext &, const QString & msg)
+{
+    QString txt;
+    switch (type) {
+        case QtDebugMsg:
+            txt = QString("Debug: %1").arg(msg);
+            break;
+        case QtWarningMsg:
+            txt = QString("Warning: %1").arg(msg);
+            break;
+        case QtCriticalMsg:
+            txt = QString("Critical: %1").arg(msg);
+            break;
+        case QtFatalMsg:
+        default:
+            txt = QString("Fatal: %1").arg(msg);
+            abort();
+    }
+    QFile outFile("cp-camera-face.log");
+    outFile.open(QIODevice::WriteOnly | QIODevice::Append);
+    QTextStream ts(&outFile);
+    ts << txt << endl;
+}
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    qInstallMessageHandler(myCustomMessageHandler);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
